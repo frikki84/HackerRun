5c1cc1cc0da7d71dbde37a8ce445264430d4b64c
Yana Dziadkouskaya <Yana.Dziadkouskaya@softeq.com>
2021-06-04 16:53:21 +0300

    Java Datatypes2

--- src/main/java/com/softeq/main/DoSomething.java
+++ src/main/java/com/softeq/main/DoSomething.java
@@ -4,19 +4,19 @@ public class DoSomething {
 
 
     public static void main(String[] args) {
-        long x = -1500000000000l;
+        long x = -150;
         try {
 
             if (x >= -128 && x <= 127) {
                 System.out.println("* byte");
             }
-            if (x >= Math.pow(2, -15) && x < Math.pow(2, 15)) {
-                System.out.println("* shot");
+            if (x >= -Math.pow(2, 15) && x < Math.pow(2, 15)) {
+                System.out.println("* short");
             }
-            if (x >= Math.pow(2, -31) && x < Math.pow(2, 31)) {
+            if (x >= -Math.pow(2, 31) && x < Math.pow(2, 31)) {
                 System.out.println("* int");
             }
-            if (x >= Math.pow(2, -63) && x < Math.pow(2, 63)) {
+            if (x >= -Math.pow(2, 63) && x < Math.pow(2, 63)) {
                 System.out.println("* long");
             }
 
