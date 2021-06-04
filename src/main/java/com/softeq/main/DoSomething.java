52c58cf09e603b4fa7e16489219c205b9bf7fa6a
Yana Dziadkouskaya <Yana.Dziadkouskaya@softeq.com>
2021-06-04 17:25:32 +0300

    +1

--- src/main/java/com/softeq/main/DoSomething.java
+++ src/main/java/com/softeq/main/DoSomething.java
@@ -1,33 +1,42 @@
 package com.softeq.main;
 
+import java.util.Scanner;
+
 public class DoSomething {
 
-
     public static void main(String[] args) {
-        long x = -150;
-        try {
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//
+//
+//        for (int i = 0; i < t; i++) {
+            try {
+                long x =(long) Math.pow(2, 7)-1;
 
-            if (x >= -128 && x <= 127) {
-                System.out.println("* byte");
-            }
-            if (x >= -Math.pow(2, 15) && x < Math.pow(2, 15)) {
-                System.out.println("* short");
-            }
-            if (x >= -Math.pow(2, 31) && x < Math.pow(2, 31)) {
-                System.out.println("* int");
-            }
-            if (x >= -Math.pow(2, 63) && x < Math.pow(2, 63)) {
-                System.out.println("* long");
-            }
+                System.out.println(x + " can be fitted in:");
 
-        } catch (Exception e) {
-            System.out.println(" can't be fitted anywhere.");
+                if (x >= -Math.pow(2, 7) && x <= Math.pow(2, 7)) {
+                    System.out.println("* byte");
+                }
+                if (x >= -Math.pow(2, 15) && x < Math.pow(2, 15)) {
+                    System.out.println("* short");
+                }
+                if (x >= -Math.pow(2, 31) && x < Math.pow(2, 31)) {
+                    System.out.println("* int");
+                }
+                if (x >= -Math.pow(2, 63) && x < Math.pow(2, 63)) {
+                    System.out.println("* long");
+                }
+            } catch (Exception e) {
+                System.out.println(" can't be fitted anywhere.");
+            }
         }
-
-
     }
+//}
 
-}
+
+
+
 
 
 
---
+++ src/main/java/com/softeq/main/Unboxer.java
@@ -0,0 +1,9 @@
+package com.softeq.main;
+
+public class Unboxer {
+    private Integer x;
+
+    public boolean compare(int y) {
+        return x == y;
+    }
+}
